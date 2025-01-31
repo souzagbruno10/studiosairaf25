7f726e9a0134435f7a04e1c90b0498100de4988e
souzagbruno10 <souzagbruno10@gmail.com>
2025-01-31 01:02:42 +0000

    arruma banco

---
+++ migrations/20250131005955_create_agendamentos/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `agendamentos` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `data` DATETIME(3) NOT NULL,
+    `hora` VARCHAR(191) NOT NULL,
+    `servico` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
